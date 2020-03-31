5de42a19b176ece85837913f84060ce37686211f
hironakazuki <far_east_nightbird30@icloud.com>
2020-03-31 15:54:32 +0900

    implement 3b Exercise 3.9

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
 const morgan = require('morgan')
 
 app.use(express.json())
+app.use(cors())
+
 app.use(morgan((tokens, req, res) => {
   console.log(req.body)
   return [
@@ -103,7 +107,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
